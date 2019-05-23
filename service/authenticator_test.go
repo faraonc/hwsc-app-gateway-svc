63e3fa1e50c0cec5a3735213f4946ea418b3a2f2
faraonc <faraonc@outlook.com>
2019-05-22 23:18:07 -0700

    Refactor case descriptions

--- service/authenticator_test.go
+++ service/authenticator_test.go
@@ -16,6 +16,7 @@ func TestAuthenticate(t *testing.T) {
 	/*
 		Test for basic authentication
 	*/
+	baseAuthCase := "test authenticating valid user"
 	validEmail := randomdata.Email()
 	validPassword := "Abcd!123@"
 	resp, errCreateUser := userSvc.createUser(
@@ -26,11 +27,11 @@ func TestAuthenticate(t *testing.T) {
 			Password:     validPassword,
 			Organization: testOrg,
 		})
-	assert.Nil(t, errCreateUser, "TestAuthenticate create valid user - no err")
-	assert.NotNil(t, resp, "TestAuthenticate create valid user - resp not nil")
+	assert.Nil(t, errCreateUser, baseAuthCase)
+	assert.NotNil(t, resp, baseAuthCase)
 	emailToken := resp.GetIdentification().GetToken()
 	errVerifyEmailToken := userSvc.verifyEmailToken(emailToken)
-	assert.Nil(t, errVerifyEmailToken, "TestAuthenticate create valid user - verify email token")
+	assert.Nil(t, errVerifyEmailToken, baseAuthCase)
 	validEnc := base64.StdEncoding.EncodeToString([]byte(validEmail + ":" + validPassword))
 
 	header := metadata.New(map[string]string{
@@ -39,32 +40,34 @@ func TestAuthenticate(t *testing.T) {
 	validIncomingCtx := metadata.NewIncomingContext(context.Background(), header)
 
 	incomingCtx1, errAuthenticate := Authenticate(validIncomingCtx)
-	assert.Nil(t, errAuthenticate, "TestAuthenticate create valid user - success")
+	assert.Nil(t, errAuthenticate, baseAuthCase)
 	incomingMd1, incomingMdOk1 := metadata.FromIncomingContext(incomingCtx1)
-	assert.True(t, incomingMdOk1, "TestAuthenticate create valid user - incoming ctx1 OK")
+	assert.True(t, incomingMdOk1, baseAuthCase)
 	incomingAuthToken1, incomingAuthTokenOk1 := incomingMd1[consts.StrMdAuthToken]
-	assert.True(t, incomingAuthTokenOk1, "TestAuthenticate create valid user - incoming auth token1 OK")
-	assert.Equal(t, 1, len(incomingAuthToken1), "TestAuthenticate create valid user - incoming auth token1")
+	assert.True(t, incomingAuthTokenOk1, baseAuthCase)
+	assert.Equal(t, 1, len(incomingAuthToken1), baseAuthCase)
 
 	// repeat Authenticate
+	baseAuthRepeatCase := "test with repeated authentication"
 	incomingCtx2, errAuthenticate := Authenticate(validIncomingCtx)
-	assert.Nil(t, errAuthenticate, "TestAuthenticate create valid user - repeat success")
+	assert.Nil(t, errAuthenticate, baseAuthRepeatCase)
 	incomingMd2, incomingMdOk2 := metadata.FromIncomingContext(incomingCtx2)
-	assert.True(t, incomingMdOk2, "TestAuthenticate create valid user - incoming ctx2 OK")
+	assert.True(t, incomingMdOk2, baseAuthRepeatCase)
 	incomingAuthToken2, incomingAuthTokenOk2 := incomingMd2[consts.StrMdAuthToken]
-	assert.True(t, incomingAuthTokenOk2, "TestAuthenticate create valid user - incoming auth token2 OK")
-	assert.Equal(t, 1, len(incomingAuthToken2), "TestAuthenticate create valid user - incoming auth token2")
+	assert.True(t, incomingAuthTokenOk2, baseAuthRepeatCase)
+	assert.Equal(t, 1, len(incomingAuthToken2), baseAuthRepeatCase)
 
 	// repeat Authenticate with new AuthSecret
+	baseAuthNewAuthSecretCase := "test authenticate with new auth secret"
 	errNewSecret1 := userSvc.makeNewAuthSecret()
-	assert.Nil(t, errNewSecret1, "TestAuthenticate create valid user - new secret 1")
+	assert.Nil(t, errNewSecret1, baseAuthNewAuthSecretCase)
 	incomingCtx3, errAuthenticate := Authenticate(validIncomingCtx)
-	assert.Nil(t, errAuthenticate, "TestAuthenticate create valid user - repeat success")
+	assert.Nil(t, errAuthenticate, baseAuthNewAuthSecretCase)
 	incomingMd3, incomingMdOk3 := metadata.FromIncomingContext(incomingCtx3)
-	assert.True(t, incomingMdOk3, "TestAuthenticate create valid user - incoming ctx3 OK")
+	assert.True(t, incomingMdOk3, baseAuthNewAuthSecretCase)
 	incomingAuthToken3, incomingAuthTokenOk3 := incomingMd3[consts.StrMdAuthToken]
-	assert.True(t, incomingAuthTokenOk3, "TestAuthenticate create valid user - incoming auth token3 OK")
-	assert.Equal(t, 1, len(incomingAuthToken3), "TestAuthenticate create valid user - incoming auth token3")
+	assert.True(t, incomingAuthTokenOk3, baseAuthNewAuthSecretCase)
+	assert.Equal(t, 1, len(incomingAuthToken3), baseAuthNewAuthSecretCase)
 
 	/*
 		Test for auth token
@@ -85,6 +88,7 @@ func TestAuthenticate(t *testing.T) {
 	/*
 		Test for email token verification
 	*/
+	tokenAuthCase := "test authenticating with valid auth token"
 	newEmailVerificationUserEmail := randomdata.Email()
 	newEmailVerificationUserPassword := "Abcd!123@"
 	newEmailVerificationResp, errNewEmailVerificationCreateUser := userSvc.createUser(
@@ -95,15 +99,15 @@ func TestAuthenticate(t *testing.T) {
 			Password:     newEmailVerificationUserPassword,
 			Organization: testOrg,
 		})
-	assert.Nil(t, errNewEmailVerificationCreateUser, "TestAuthenticate email token verification - no err")
-	assert.NotNil(t, newEmailVerificationResp, "TestAuthenticate email token verification - resp not nil")
+	assert.Nil(t, errNewEmailVerificationCreateUser, tokenAuthCase)
+	assert.NotNil(t, newEmailVerificationResp, tokenAuthCase)
 	newEmailVerificationToken := newEmailVerificationResp.GetIdentification().GetToken()
 	newEmailVerificationHeader := metadata.New(map[string]string{
 		consts.StrMdBasicAuthHeader: consts.StrEmailTokenVerificationPrefix + newEmailVerificationToken,
 	})
 	newEmailVerificationOutgoingCtx := metadata.NewIncomingContext(context.Background(), newEmailVerificationHeader)
 	_, errNewEmailVerification := Authenticate(newEmailVerificationOutgoingCtx)
-	assert.Nil(t, errNewEmailVerification, "TestAuthenticate email token verification - success")
+	assert.Nil(t, errNewEmailVerification, tokenAuthCase)
 
 	/*
 		Test for error cases
@@ -201,9 +205,10 @@ func TestAuthenticate(t *testing.T) {
 		}
 	}
 
+	nilIncomingCtxCase := "test for nil incoming context"
 	ctxNil, errNil := Authenticate(nil)
-	assert.EqualError(t, errNil, consts.ErrNilContext.Error(), "test for nil incoming context")
-	assert.Equal(t, context.TODO(), ctxNil, "test for context todo")
+	assert.EqualError(t, errNil, consts.ErrNilContext.Error(), nilIncomingCtxCase)
+	assert.Equal(t, context.TODO(), ctxNil, nilIncomingCtxCase)
 }
 
 func TestTryEmailTokenVerification(t *testing.T) {
@@ -223,12 +228,13 @@ func TestFinalizeAuth(t *testing.T) {
 }
 
 func TestExtractContextHeader(t *testing.T) {
-	// test for nil context
+	nilIncomingCtxCase := "test for nil incoming context"
 	actOutput, errNil := extractContextHeader(nil, "")
-	assert.EqualError(t, errNil, consts.ErrNilContext.Error(), "test for nil incoming context")
-	assert.Zero(t, actOutput, "test for empty string")
+	assert.EqualError(t, errNil, consts.ErrNilContext.Error(), nilIncomingCtxCase)
+	assert.Zero(t, actOutput, nilIncomingCtxCase)
 
 	// test for wrong context by using OutgoingCtx
+	invalidOutgoingCtxCase := "test for invalid context using OutgoingCtx instead of IncomingCtx"
 	validEmail := randomdata.Email()
 	validPassword := "Abcd!123@"
 	resp, errCreateUser := userSvc.createUser(
@@ -239,11 +245,11 @@ func TestExtractContextHeader(t *testing.T) {
 			Password:     validPassword,
 			Organization: testOrg,
 		})
-	assert.Nil(t, errCreateUser, "TestAuthenticate create valid user - no err")
-	assert.NotNil(t, resp, "TestAuthenticate create valid user - resp not nil")
+	assert.Nil(t, errCreateUser, invalidOutgoingCtxCase)
+	assert.NotNil(t, resp, invalidOutgoingCtxCase)
 	emailToken := resp.GetIdentification().GetToken()
 	errVerifyEmailToken := userSvc.verifyEmailToken(emailToken)
-	assert.Nil(t, errVerifyEmailToken, "TestAuthenticate create valid user - verify email token")
+	assert.Nil(t, errVerifyEmailToken, invalidOutgoingCtxCase)
 	validEnc := base64.StdEncoding.EncodeToString([]byte(validEmail + ":" + validPassword))
 
 	header := metadata.New(map[string]string{
@@ -251,18 +257,17 @@ func TestExtractContextHeader(t *testing.T) {
 	})
 	invalidOutgoingCtx := metadata.NewOutgoingContext(context.Background(), header)
 	actualOutput1, err := extractContextHeader(invalidOutgoingCtx, consts.StrMdBasicAuthHeader)
-	assert.Zero(t, actualOutput1, "test for wrong context by using OutgoingCtx")
-	assert.EqualError(t, err, "rpc error: code = Unauthenticated desc = no headers in request",
-		"test for wrong context by using OutgoingCtx")
+	assert.Zero(t, actualOutput1, invalidOutgoingCtxCase)
+	assert.EqualError(t, err, "rpc error: code = Unauthenticated desc = no headers in request", invalidOutgoingCtxCase)
 
-	// test for invalid header
+	invalidHeaderCase := "test for authorization invalid header"
 	validIncomingCtx1 := metadata.NewIncomingContext(context.Background(), header)
 	actualOutput2, err := extractContextHeader(validIncomingCtx1, placeholder)
-	assert.Zero(t, actualOutput2, "test for invalid header")
+	assert.Zero(t, actualOutput2, invalidHeaderCase)
 	assert.EqualError(t, err, "rpc error: code = Unauthenticated desc = no \"authorization\" header in request",
-		"test for invalid header")
+		invalidHeaderCase)
 
-	// test for multi headers
+	multiHeaderCase := "test for multi authorization headers"
 	md1 := metadata.New(map[string]string{
 		consts.StrMdBasicAuthHeader: consts.StrBasicAuthPrefix + validEnc,
 	})
@@ -272,15 +277,13 @@ func TestExtractContextHeader(t *testing.T) {
 	multiHeader := metadata.Join(md1, md2)
 	invalidIncomingCtx2 := metadata.NewIncomingContext(context.Background(), multiHeader)
 	actualOutput3, err := extractContextHeader(invalidIncomingCtx2, consts.StrMdBasicAuthHeader)
-	assert.Zero(t, actualOutput3, "test for wrong context by using multiple headers")
-	assert.EqualError(t, err, "rpc error: code = Unauthenticated desc = more than 1 header in request",
-		"test for wrong context by using multiple headers")
+	assert.Zero(t, actualOutput3, multiHeaderCase)
+	assert.EqualError(t, err, "rpc error: code = Unauthenticated desc = more than 1 header in request", multiHeaderCase)
 
 	cases := []struct {
 		desc       string
 		authHeader string
 		authPrefix string
-		mdKey      string
 		input      string
 		isExpErr   bool
 		errStr     string
@@ -289,7 +292,6 @@ func TestExtractContextHeader(t *testing.T) {
 			"test missing auth header",
 			"",
 			consts.StrBasicAuthPrefix,
-			consts.StrMdAuthToken,
 			randomdata.Email() + ":" + "Qwert!123@",
 			true,
 			"rpc error: code = Unauthenticated desc = missing header",
@@ -298,7 +300,6 @@ func TestExtractContextHeader(t *testing.T) {
 			"test missing auth header",
 			consts.StrMdBasicAuthHeader,
 			consts.StrBasicAuthPrefix,
-			consts.StrMdAuthToken,
 			randomdata.Email() + ":" + "Qwert!123@",
 			false,
 			"",
